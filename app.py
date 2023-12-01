d14a478fccc4963adfb45ad96b8afe6d46c2df04
jabu <jbllives@gmail.com>
2023-12-01 09:56:32 +0200

    Inclucing the mysql connector

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask
+import mysql.connector
+
+
 app = Flask(__name__)
+
+conn = mysql.connector.connect(
+    host="192.168.31.100",
+    user="root",
+    password="jabu@1994",
+    database="tutorials"
+)
 
 @app.route('/')
 def hello_world():
