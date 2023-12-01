e82dfe6b0ca1c74dc3843bc596fefd594e0be7fb
jabu <jbllives@gmail.com>
2023-12-01 13:55:35 +0200

    Adding Functionalities for CRUD

--- app.py
+++ app.py
@@ -14,7 +14,56 @@ conn = mysql.connector.connect(
 )
 
 mycursor = conn.cursor()
+@app.route('/insert_games', methods=['POST'])
+def insert_games():
+    try:
+        g_name = request.form.get('gameName')
+        g_price = int(request.form.get('gamePrice'))
+        g_date = request.form.get('gameDate')
+        g_rating = request.form.get('gameRating')
 
+        sql = "INSERT INTO games (game_name, game_price, game_date, game_rating) VALUES (%s, %s, %s, %s)"
+        val = (g_name, g_price, g_date, g_rating)
+
+        mycursor.execute(sql, val)
+        conn.commit()
+        return jsonify({'message': 'Game data inserted successfully'}), 201
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+# Function to retrieve a single data set of game based on the user input
+@app.route('/get_game', methods=['GET', 'POST'])
+def get_game():
+    try:
+        if request.method == 'GET':
+            g_name = request.args.get('gameName')
+        elif request.method == 'POST':
+            g_name = request.form.get('gameName')
+        else:
+            return jsonify({'message': 'Method not allowed'}), 405
+
+        sql = "SELECT * FROM games WHERE game_name = %s"
+        mycursor.execute(sql, (g_name,))
+
+        game_data = mycursor.fetchone()
+
+        if game_data:
+            return jsonify({
+                'game_name': game_data[1],
+                'price': str(game_data[2]),
+                'rating': str(game_data[3]),
+                'release_date': str(game_data[4])
+            }), 200
+        else:
+            return jsonify({'message': 'Game not found'}), 404
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+# Function to retrieve all games from the database
 @app.route('/get_all_games', methods=['GET'])
 def get_all_games():
     try:
