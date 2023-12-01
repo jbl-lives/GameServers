7481631f9ad0a857e77c999659ca648815dba9b1
jabu <jbllives@gmail.com>
2023-12-01 13:47:55 +0200

    Adding database get function to see if it works

--- app.py
+++ app.py
@@ -15,7 +15,26 @@ conn = mysql.connector.connect(
 
 mycursor = conn.cursor()
 
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
+@app.route('/get_all_games', methods=['GET'])
+def get_all_games():
+    try:
+        sql = "SELECT * FROM games"
+        mycursor.execute(sql)
+
+        games_data = mycursor.fetchall()
+
+        games_list = []
+
+        for game_data in games_data:
+            game = {
+                'game_name': game_data[1],
+                'price': str(game_data[2]),
+                'rating': str(game_data[3]),
+                'release_date': str(game_data[4])
+            }
+            games_list.append(game)
+
+        return jsonify({'games': games_list}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
